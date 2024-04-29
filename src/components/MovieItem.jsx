3e284172b903831e76d1fdc9d9cc6d81a759bd0f
SawantAchal <achals1718@gmail.com>
2024-04-29 17:55:09 +0530

    add style

--- src/components/MovieItem.jsx
+++ src/components/MovieItem.jsx
@@ -11,7 +11,7 @@ const MovieItem = ({movie}) => {
   return (
     <div className="movie-item flex items-center justify-center  flex-col cursor-pointer border border-white gap-6 text-center p-3 rounded-lg" onClick={handleClick}>
       <img src={moviemainphotos} alt={movietitle} className='text-center w-full rounded-lg'/>
-        <h2 className='bg-transparent border border-white text-2xl w-full p-3 text-white rounded-lg'>{movietitle}</h2>
+        <h2 className='bg-transparent   text-2xl w-full p-3 text-white rounded-lg'>{movietitle}</h2>
     </div>
   )
 }
